b0fbbf78cc3f2943c2623fa687e8423e08dbc12c
hol3k <admin@filipholowko.pl>
2019-05-15 13:31:39 +0200

    Pierwszy commit

---
+++ src/com/jc/main/Main.java
@@ -0,0 +1,16 @@
+package com.jc.main;
+
+/**
+ * Main class.
+ */
+public class Main {
+
+    /**
+     * Main method
+     * @param args arguments
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
